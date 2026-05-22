fcd48eb932999715cf82ba9c13d0fca33a698208
Saksham Agrahari <agrahari0899@gmail.com>
2026-05-22 12:14:41 +0530

    Added the Angular Day3 Data Binding Concept

--- Angular/AngularDemo1/src/app/about/about.ts
+++ Angular/AngularDemo1/src/app/about/about.ts
@@ -1,9 +1,60 @@
+import { NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: '[app-about]',
-  imports: [],
+  imports: [NgClass, NgStyle, FormsModule],
   templateUrl: './about.html',
-  styleUrl: './about.css',
+  styleUrls: ['./about.css'],
 })
-export class About {}
+
+export class About {
+
+  currentCustomer = {
+    name: 'John Doe',
+    email: 'john@example.com',
+    phone: '123-456-7890',
+    address: '123 Main St, AnyTown, USA'
+  };
+
+  cities = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix'];
+
+  // property Binding
+  imgPath = '/favicon.ico';
+
+  // Attribute Binding
+  isButtonDisabled = true;
+
+  updateButton() {
+    this.isButtonDisabled = !this.isButtonDisabled;
+    const button = document.getElementById('toggleButton');
+    if (button) {
+      button.innerHTML = this.isButtonDisabled ? 'Enable Button' : 'Disable Button';
+    }
+  }
+
+  // class Binding
+  isDivVisible = false;
+  toggleDivVisibility() {
+    this.isDivVisible = !this.isDivVisible;
+  }
+
+  // style Binding
+  btnStyle = 'margin: 10px; padding: 10px; background-color: green; border: none; cursor: pointer;';
+
+
+
+  // event Binding  
+  inputValue = '**';
+  handleInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.inputValue = input.value;
+    console.log('Input value:', input.value);
+  }
+
+  // Two way Binding
+  username = 'Test User';
+  userCity = 'New York';
+}
